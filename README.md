233d9a321d900a47da69c676c5bc710d11ab538f
filippodaniotti <47757310+filippodaniotti@users.noreply.github.com>
2020-09-23 20:26:05 +0200

    :memo: First commit
    con l'impalcatura di b0zzo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Appunti-LFC
+Dispensa assolutamente in corso d'opera per il corso di
+
+>LINGUAGGI INFERNALI E COMPILATORI
+
+Tutta l'impalcatura è ~~ispirata~~ spudoratamente copiata da quella di frabozzo perché sono un diocane (e comunque conoscete altro modo di lavorare?)
---
+++ src/chapters/compile-chapters.bat
@@ -0,0 +1,39 @@
+cd 01
+arara -v chapter01
+cd ..
+pause
+
+cd 02
+arara -v chapter02
+cd ..
+pause
+
+cd 03
+arara -v chapter03
+cd ..
+pause
+
+cd 04
+arara -v chapter04
+cd ..
+pause
+
+cd 05
+arara -v chapter05
+cd ..
+pause
+
+cd 06
+arara -v chapter06
+cd ..
+pause
+
+cd 07
+arara -v chapter07
+cd ..
+pause
+
+cd 08
+arara -v chapter08
+cd ..
+pause
